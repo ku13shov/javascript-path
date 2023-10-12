0fa12c6cdbe3d74fa3e619a6f12ef5839ed7ca49
Пользователь <kuleshov000@gmail.com>
2023-10-12 19:05:06 +0300

    add desktop

---
+++ app/js/main.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const selectCurrent = document.querySelector('.rating__select-current');
+const selectCurrentText = document.querySelector('.rating__select-current span');
+const selectArrow = document.querySelector('.rating__select-current img');
+const selectItems = document.querySelector('.rating__select-items');
+
+const toggleClassActive = (element) => {
+    element.classList.toggle('active');
+}
+
+selectCurrent.addEventListener('click', () => {
+    toggleClassActive(selectItems);
+    toggleClassActive(selectArrow);
+});
+
+selectItems.addEventListener('click', (e) => {
+    toggleClassActive(selectItems);
+    toggleClassActive(selectArrow);
+
+    selectCurrentText.textContent = e.target.textContent;
+    selectCurrent.classList.add('active');
+});
+
+const swiper = new Swiper('.swiper', {
+    // loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        440: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
